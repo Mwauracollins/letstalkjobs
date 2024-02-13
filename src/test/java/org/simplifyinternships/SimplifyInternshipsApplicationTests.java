dd28bef4b09855fab305a8308528ffebba3329eb
Mwauracollins <collinsmwaura8844@gmail.com>
2024-02-14 00:49:21 +0300

    Initial commit

---
+++ src/test/java/org/simplifyinternships/SimplifyInternshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simplifyinternships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifyInternshipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
