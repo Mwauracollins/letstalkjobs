28c8abfbab5689923e60de44165c0ee198db7336
Mwauracollins <collinsmwaura8844@gmail.com>
2024-02-24 13:25:27 +0300

    Initial commit

---
+++ src/main/java/org/simplifyinternships/simplifyinternships/SimplifyInternshipsApplication.java
@@ -0,0 +1,13 @@
+package org.simplifyinternships.simplifyinternships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyInternshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplifyInternshipsApplication.class, args);
+    }
+
+}
