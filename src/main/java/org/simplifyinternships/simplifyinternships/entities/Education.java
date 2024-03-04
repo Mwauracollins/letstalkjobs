fad9492ec9e77486ee6a931668a1340f40912ad4
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-04 15:05:36 +0300

    Records the education attr of a user

---
+++ src/main/java/org/simplifyinternships/simplifyinternships/entities/Education.java
@@ -0,0 +1,46 @@
+package org.simplifyinternships.simplifyinternships.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "education")
+public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "education_id")
+    private Integer educationId;
+    @Getter
+    @Setter
+    @Column(name = "major")
+    private String major;
+    @Getter
+    @Setter
+    @Column(name = "university")
+    private String university;
+    @Getter
+    @Setter
+    @Column(name = "start_year")
+    private String startYear;
+    @Getter
+    @Setter
+    @Column(name = "end_year")
+    private String endYear;
+    @Getter
+    @Setter
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private BaseUser user;
+
+    public Education() {
+    }
+
+    public Education(String major, String university, String startYear, String endYear) {
+        this.major = major;
+        this.university = university;
+        this.startYear = startYear;
+        this.endYear = endYear;
+    }
+
+
+}
