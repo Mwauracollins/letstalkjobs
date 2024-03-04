59ae7f66517858e266a3b096ce5a576eb2addebe
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-04 15:02:02 +0300

    Created the chat entity for all user conversations

---
+++ src/main/java/org/simplifyinternships/simplifyinternships/entities/Chat.java
@@ -0,0 +1,23 @@
+package org.simplifyinternships.simplifyinternships.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "chat")
+public class Chat {
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "chat_id")
+    private Integer chatId;
+    @Getter
+    @Setter
+    @Column(name = "message")
+    private String message;
+
+    public Chat() {
+    }
+}
