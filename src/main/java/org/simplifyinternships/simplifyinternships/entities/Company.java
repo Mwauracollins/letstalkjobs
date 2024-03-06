324778bd534835ce0f24cb07525f547f3c03c609
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-06 20:31:19 +0300

    Minor changes to relationsip description

--- src/main/java/org/simplifyinternships/simplifyinternships/entities/Company.java
+++ src/main/java/org/simplifyinternships/simplifyinternships/entities/Company.java
@@ -3,6 +3,10 @@ package org.simplifyinternships.simplifyinternships.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.simplifyinternships.simplifyinternships.entities.jobentities.Attachment;
+import org.simplifyinternships.simplifyinternships.entities.jobentities.Internship;
+
+import java.util.List;
 
 @Entity
 @Table(name = "company")
@@ -12,10 +16,28 @@ public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "company_id")
-    private Integer id;
-    @OneToOne(cascade = CascadeType.ALL)
+    private Integer companyId;
+    @Getter
+    @Setter
+    @Column(name = "description")
+    private String companyDescription;
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "company")
     @JoinColumn(name = "company_id")
     private ContactInformation contactInformation;
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<Internship> internships;
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<Attachment> attachment;
+
+    public Company(){
+
+    }
 
 
 }
