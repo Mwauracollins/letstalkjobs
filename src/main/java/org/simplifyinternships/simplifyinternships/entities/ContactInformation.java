07d26281c27c3668f51bc9342ae14f2b26590996
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-06 20:28:46 +0300

    Added relationship to a User or a Compan

--- src/main/java/org/simplifyinternships/simplifyinternships/entities/ContactInformation.java
+++ src/main/java/org/simplifyinternships/simplifyinternships/entities/ContactInformation.java
@@ -3,6 +3,12 @@ package org.simplifyinternships.simplifyinternships.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.simplifyinternships.simplifyinternships.entities.userentities.BaseUser;
+
+/*
+THis records all the contact information about eh user and the companies. The
+visibility specifies if the details are visible to the public or hidden.
+ */
 
 @Entity
 @Table(name = "contact_information")
@@ -12,12 +18,45 @@ public class ContactInformation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "contact_information_id")
-    private Integer id;
+    private Integer contactInformationId;
+    @Getter
+    @Setter
+    @Column(name = "email")
     private String email;
+    @Getter
+    @Setter
+    @Column(name = "email_visibility")
     private Boolean email_visibility;
+    @Getter
+    @Setter
+    @Column(name = "phone_number")
     private String phone_number;
+    @Getter
+    @Setter
+    @Column(name = "phone_number_visibility")
     private Boolean phone_number_visibility;
+    @Getter
+    @Setter
+    @Column(name = "website")
     private String website;
+    @Getter
+    @Setter
+    @Column(name = "website_visibility")
     private Boolean website_visibility;
+
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private BaseUser user;
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    public ContactInformation(){
+
+    }
 
 }
