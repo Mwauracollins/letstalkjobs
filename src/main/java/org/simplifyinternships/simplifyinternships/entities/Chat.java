778d29913c853fb70c9fd4f94bb4172e55b5daf2
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-08 14:59:08 +0300

    Defined relationships with the sender and receiver

--- src/main/java/org/simplifyinternships/simplifyinternships/entities/Chat.java
+++ src/main/java/org/simplifyinternships/simplifyinternships/entities/Chat.java
@@ -3,6 +3,7 @@ package org.simplifyinternships.simplifyinternships.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.simplifyinternships.simplifyinternships.entities.userentities.BaseUser;
 
 @Entity
 @Table(name = "chat")
@@ -17,6 +18,21 @@ public class Chat {
     @Setter
     @Column(name = "message")
     private String message;
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "sender_user_id")
+    private BaseUser sender;
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "receiver_user_id")
+    private BaseUser receiver;
+    @Getter
+    @Setter
+    @OneToOne(mappedBy = "chat", cascade = CascadeType.ALL)
+    private Notification chatMeta;
+
 
     public Chat() {
     }
