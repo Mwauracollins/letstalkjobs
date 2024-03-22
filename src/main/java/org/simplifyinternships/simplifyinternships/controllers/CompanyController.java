2a4f1473a824b5b4971f8c1a20c916ded1368cf7
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-22 23:46:50 +0300

    Changed return type

--- src/main/java/org/simplifyinternships/simplifyinternships/controllers/CompanyController.java
+++ src/main/java/org/simplifyinternships/simplifyinternships/controllers/CompanyController.java
@@ -1,27 +1,43 @@
 package org.simplifyinternships.simplifyinternships.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.simplifyinternships.simplifyinternships.dto.requests.CompanyRequest;
+import org.simplifyinternships.simplifyinternships.dto.requests.JobOpportunityRequest;
+import org.simplifyinternships.simplifyinternships.dto.responses.CompanyResponse;
+import org.simplifyinternships.simplifyinternships.dto.responses.JobOpportunityResponse;
 import org.simplifyinternships.simplifyinternships.entities.Company;
 import org.simplifyinternships.simplifyinternships.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
+@RequiredArgsConstructor
 public class CompanyController {
     private final CompanyService companyService;
 
-    @Autowired
-    public CompanyController(CompanyService companyService) {
-        this.companyService = companyService;
+    @RequestMapping(value = "/companies", method = RequestMethod.GET)
+    public ResponseEntity<List<CompanyResponse>> getAllCompanies(){
+        List<CompanyResponse> companyResponses = companyService.getAllCompanies();
+        return ResponseEntity.ok(companyResponses);
     }
-    @GetMapping
-    public List<Company> getAllCompanies(){
-        return companyService.getAllCompanies();
+    @PostMapping("/add-company")
+    public ResponseEntity<CompanyResponse> addCompany(
+            @RequestBody CompanyRequest request
+    ){
+        return ResponseEntity.ok(companyService.addCompany(request));
     }
-    @PostMapping
-    public Company addCompany(@RequestBody Company company){
-        return companyService.addCompany(company);
+    @GetMapping("/jobs")
+    public ResponseEntity<List<JobOpportunityResponse>> getAllCompanyJobs(
+            @RequestParam("companyName") String companyName
+    ){
+        CompanyRequest companyRequest = new CompanyRequest();
+        companyRequest.setCompanyName(companyName);
+
+        List<JobOpportunityResponse> opportunityResponses = companyService.getCompanyJobs(companyRequest);
+        return ResponseEntity.ok(opportunityResponses);
     }
 }
