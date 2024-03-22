4dc4306ba0a9c7c695b730c1f4f79f459f6423dc
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-22 23:49:50 +0300

    Minor updates on return type

--- src/main/java/org/simplifyinternships/simplifyinternships/services/CompanyService.java
+++ src/main/java/org/simplifyinternships/simplifyinternships/services/CompanyService.java
@@ -1,24 +1,58 @@
 package org.simplifyinternships.simplifyinternships.services;
 
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.simplifyinternships.simplifyinternships.dto.requests.CompanyRequest;
+import org.simplifyinternships.simplifyinternships.dto.requests.JobOpportunityRequest;
+import org.simplifyinternships.simplifyinternships.dto.responses.CompanyResponse;
+import org.simplifyinternships.simplifyinternships.dto.responses.JobOpportunityResponse;
 import org.simplifyinternships.simplifyinternships.entities.Company;
+import org.simplifyinternships.simplifyinternships.entities.jobentities.JobOpportunity;
 import org.simplifyinternships.simplifyinternships.repositories.CompanyRepository;
+import org.simplifyinternships.simplifyinternships.repositories.JobOpportunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CompanyService {
-    private final CompanyRepository companyRepository;
 
-    @Autowired
-    public CompanyService(CompanyRepository companyRepository) {
-        this.companyRepository = companyRepository;
+    private final CompanyRepository companyRepository;
+    private final JobOpportunityRepository jobOpportunityRepository;
+
+    public List<CompanyResponse> getAllCompanies(){
+        return companyRepository.findAll()
+                .stream()
+                .map(company -> CompanyResponse.builder()
+                        .build()
+                )
+                .collect(Collectors.toList());
     }
-    public List<Company> getAllCompanies(){
-        return companyRepository.findAll();
+    public CompanyResponse addCompany(@NotNull CompanyRequest request){
+        var company = Company.builder()
+                .companyName(request.getCompanyName())
+                .companyDescription(request.getCompanyDescription())
+                .build();
+
+        companyRepository.save(company);
+        return CompanyResponse.builder().build();
     }
-    public Company addCompany(Company company){
-        return companyRepository.save(company);
+    public List<JobOpportunityResponse> getCompanyJobs(@NotNull CompanyRequest companyRequest) {
+        Company company = companyRepository.findByCompanyName(companyRequest.getCompanyName());
+
+        if (company == null) {
+            return Collections.emptyList();
+        }
+        return jobOpportunityRepository.findByCompany(company)
+                .stream()
+                .map(jobOpportunity -> JobOpportunityResponse.builder()
+                        .build()
+                )
+                .collect(Collectors.toList());
     }
 }
