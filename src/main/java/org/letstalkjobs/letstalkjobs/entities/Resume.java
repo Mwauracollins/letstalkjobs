ec90e57aa16769cc350c42135647d5475dd3034b
Mwauracollins <collinsmwaura8844@gmail.com>
2024-04-01 01:39:25 +0300

    Added resume entity

---
+++ src/main/java/org/letstalkjobs/letstalkjobs/entities/Resume.java
@@ -0,0 +1,27 @@
+package org.letstalkjobs.letstalkjobs.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import org.letstalkjobs.letstalkjobs.entities.userentities.Applicant;
+
+import java.io.File;
+
+@Entity
+@Table(name = "resume")
+public class Resume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "resume_id")
+    private Integer resumeId;
+    @Column(name = "document")
+    private File document;
+    @OneToOne
+    @JoinColumn(name = "applicant_id")
+    private Applicant applicant;
+}
