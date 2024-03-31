c095478d79e7be9bfd1c439d10472e922428b6be
Mwauracollins <collinsmwaura8844@gmail.com>
2024-04-01 02:29:50 +0300

    defined isAuthenticated -> Check if a user has been authenticated and redirects if so

--- src/main/java/org/letstalkjobs/letstalkjobs/controllers/auth/AuthenticationController.java
+++ src/main/java/org/letstalkjobs/letstalkjobs/controllers/auth/AuthenticationController.java
@@ -6,10 +6,15 @@ import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.letstalkjobs.letstalkjobs.auth.*;
+import org.letstalkjobs.letstalkjobs.config.JwtService;
 import org.letstalkjobs.letstalkjobs.config.LogoutService;
+import org.letstalkjobs.letstalkjobs.entities.userentities.BaseUser;
+import org.letstalkjobs.letstalkjobs.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.security.Principal;
 
 /*
 TODO:Add logout
@@ -28,10 +34,19 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     private final LogoutService logoutService;
+    private final JwtService jwtService;
+    private UserService userService;
 
     @GetMapping(value = "/register")
-    public String showRegisterPage(@NotNull Model model){
+    public String showRegisterPage(
+            @NotNull Model model,
+            HttpServletRequest request,
+            Principal connectedUser){
         model.addAttribute("message", "Welcome to the register page");
+//        if (isAuthenticated(request, connectedUser)){
+//
+//            return "redirect:/home";
+//        }
         return "register";
     }
 
@@ -47,20 +62,33 @@ public class AuthenticationController {
     }
     @GetMapping(value = "/login")
     public String showLoginPage(
-            @NotNull Model model
+            @NotNull Model model,
+            HttpServletRequest request,
+            Principal connectedUser
     ){
         model.addAttribute("message", "Welcome to the register page");
+        if (isAuthenticated(request, connectedUser)){
+
+            return "redirect:/home";
+        }
         return "login";
     }
 
     @PostMapping(value = "/login")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<?> authenticate(
             @RequestBody AuthenticationRequest request,
-            HttpServletResponse response
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse,
+            Principal connectedUser
     ){
+        if (isAuthenticated(httpServletRequest, connectedUser)){
+            return ResponseEntity.ok("redirect:/home");
+        }
         AuthenticationResponse authenticationResponse = authenticationService.authenticate(request);
-        response.addCookie(createCookie("access_token", authenticationResponse.getAccessToken()));
-        response.addCookie(createCookie("refresh_token", authenticationResponse.getRefreshToken()));
+        httpServletResponse.addCookie(createCookie("access_token", authenticationResponse.getAccessToken()));
+        httpServletResponse.addCookie(createCookie("refresh_token", authenticationResponse.getRefreshToken()));
+
+        var email = jwtService.extractUsername(authenticationResponse.getAccessToken());
 
         return ResponseEntity.ok(authenticationResponse);
     }
@@ -79,10 +107,31 @@ public class AuthenticationController {
             return "redirect:/auth/login";
         }
     }
-    private boolean isAuthenticated() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication != null && authentication.isAuthenticated();
+    private boolean isAuthenticated(HttpServletRequest request, Principal connectedUser) {
+        var user = (BaseUser) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        if (((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal() == null){
+            return false;
+        }
 
+        Cookie[] cookies = request.getCookies();
+        String accessToken = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("access_token".equals(cookie.getName())) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }else {
+            return false;
+        }
+        if (accessToken == null) {
+            return false;
+        }
+        if (!jwtService.isTokenValid(accessToken, user)) {
+            return false;
+        }
+        return true;
     }
 
     @PostMapping(value = "/refresh-token")
