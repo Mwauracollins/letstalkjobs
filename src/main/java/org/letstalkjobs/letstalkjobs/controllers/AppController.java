3679052114eeb572efde07074bbdba56aa706972
Mwauracollins <collinsmwaura8844@gmail.com>
2024-03-31 22:07:00 +0300

    fixed mapping: for template rendering

--- src/main/java/org/letstalkjobs/letstalkjobs/controllers/AppController.java
+++ src/main/java/org/letstalkjobs/letstalkjobs/controllers/AppController.java
@@ -1,20 +1,36 @@
 package org.letstalkjobs.letstalkjobs.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.letstalkjobs.letstalkjobs.dto.responses.JobOpportunityResponse;
+import org.letstalkjobs.letstalkjobs.services.JobOpportunityService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
+@RequiredArgsConstructor
 public class AppController {
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(){
+    private final JobOpportunityService jobOpportunityService;
+    @GetMapping
+    public String index(
+            Model model
+    ){
+        List<JobOpportunityResponse> jobs = jobOpportunityService.getAllJobs();
+        model.addAttribute("jobs", jobs);
         return "index";
     }
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String homepage(){
-        return "homepage";
+    public String homepage(
+            Model model
+    ){
+        List<JobOpportunityResponse> jobs = jobOpportunityService.getAllJobs();
+        model.addAttribute("jobs", jobs);
+        return "index";
     }
     @GetMapping(value = "/about", name = "about")
     public String about(){
