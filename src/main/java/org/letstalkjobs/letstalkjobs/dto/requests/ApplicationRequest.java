06fe1c0effc7b1299072da92ae30f6a1f44205af
Mwauracollins <collinsmwaura8844@gmail.com>
2024-04-01 00:24:50 +0300

    Minor updates

--- src/main/java/org/letstalkjobs/letstalkjobs/dto/requests/ApplicationRequest.java
+++ src/main/java/org/letstalkjobs/letstalkjobs/dto/requests/ApplicationRequest.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.letstalkjobs.letstalkjobs.Utils.ApplicationStatus;
 import org.letstalkjobs.letstalkjobs.entities.jobentities.Application;
+import org.letstalkjobs.letstalkjobs.entities.jobentities.JobOpportunity;
+import org.letstalkjobs.letstalkjobs.entities.userentities.Applicant;
+
+import java.util.Date;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApplicationRequest {
-    private Application application;
+    private ApplicationStatus applicationStatus;
+    private Date applicationDate;
+    private JobOpportunity jobOpportunity;
+    private Applicant applicant;
 }
