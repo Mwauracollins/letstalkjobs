68e5d6b38093e9d1ec26716a40728b436f54694c
Mwauracollins <collinsmwaura8844@gmail.com>
2024-04-04 11:28:18 +0300

    Cleanup

--- src/main/java/org/letstalkjobs/letstalkjobs/entities/postentities/Post.java
+++ src/main/java/org/letstalkjobs/letstalkjobs/entities/postentities/Post.java
@@ -1,34 +1,44 @@
 package org.letstalkjobs.letstalkjobs.entities.postentities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
 import org.letstalkjobs.letstalkjobs.entities.userentities.BaseUser;
 
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "posts")
 public class Post {
-    @Getter
-    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Integer postId;
-    @Getter
-    @Setter
     @Column(name = "content")
     private String content;
-    @Getter
-    @Setter
     @Column(name = "post_date")
     private String postDate;
-    @Getter
-    @Setter
     @ManyToOne
     @JoinColumn(name = "user_id")
     private BaseUser user;
+    @OneToMany(mappedBy = "post")
+    private List<PostReaction> postReactions;
 
-    public Post() {
-    }
 
 }
